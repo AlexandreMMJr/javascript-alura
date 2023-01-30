5b694a1a94f38f6358e4065fe33c23f22af87af9
AlexandreMMJr <martins.moreira.jr@gmail.com>
2023-01-30 11:28:30 -0300

    fundamentos-js

---
+++ fundamentos/type-boolean.js
@@ -0,0 +1,16 @@
+const numeroUm = 5;
+const numeroDois = 2;
+
+console.log(numeroUm === numeroDois)
+
+const cadastroAtivado = true;
+
+const textoUm = "Carlos";
+const textoDois = "Matheus";
+
+console.log(textoUm != textoDois)
+
+const palavraUm = "Alura";
+const palavraDois = "alura";
+
+console.log(palavraUm === palavraDois)
---
+++ fundamentos/type-number.js
@@ -0,0 +1,26 @@
+// tipo Number
+
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const numeroPontoFlutuante2 = .5;
+
+const operacaoFlutuante = numeroPontoFlutuante + numeroPontoFlutuante2;
+
+console.log(operacaoFlutuante)
+
+// NaN -> Not a Number
+
+const alura = "Alura";
+
+const notanumber = primeiroNumero * alura;
+
+console.log(notanumber)
